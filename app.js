642176bda34a8b80e1502d661fe02ec0800d4a7a
Jessé <jsslima22@hotmail.com>
2025-01-22 20:14:19 -0300

    Consegui fazer o botão de sortear funcionar

--- app.js
+++ app.js
@@ -2,55 +2,47 @@
 // Array para armazenar os nomes dos amigos
 let amigos = [];
 
-// Função para adicionar um amigo à lista
-function adicionarAmigo(params) {
+//Função para adicionar um amigo à lista
+function adicionarAmigo() {
     const input = document.getElementById('amigo');
     const nome = input.value.trim();
-
-    if (nome !== '') {
-        amigos.push(nome);
-        atualizarLista();
-        input.value = '';
-    }
     
+    if (nome === ''){
+        alert ('Por favor insira um nome válido');
+        return; // Interrompe a execução caso o nome seja válido
+    }
+    amigos.push(nome);
+    atualizarLista();
+    input.value = '';
 }
-// Função para atualizar a lista de amigos 
+
+//Função para atualizar a lista de amigos
 function atualizarLista() {
     const listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = '';
 
-    amigos.forEach((amigo, index) => {
+    amigos.forEach(amigo => {
         const li = document.createElement('li');
-        li.textContent = amigo; 
+        li.textContent = amigo;
         listaAmigos.appendChild(li);
     });
     
 }
-// Função para sortear o amigo secreto
+
+//Função para sortear um único amigo secreto
+
 function sortearAmigo() {
     const resultado = document.getElementById('resultado');
     resultado.innerHTML = '';
-    if (amigos.length <2){
-        alert('Você precisa adicionar pelo menos dois amigos para realizar o sorteio;');
+
+    if (amigos.length < 2) {
+        alert('Você precisa adicionar pelo menos dois amigos para realizar o sorteio');
         return;
     }
-    
-}
-let amigosRestantes = [...amigos];
-let sorteio = {};
-amigos.forEach(amigo =>{
-    let indice = Math.floor(Math.random()* amigosRestantes.length);
-    while (amigo === amigosRestantes[indice]) {
-        indice = Math.floor(Math.random()* amigosRestantes.length);
-    }
-    sorteio[amigo] = amigosRestantes[indice];
-    amigosRestantes.splice(indice, 1);
-});
-for (const[amigo,sorteado]of Object.entries(sorteio)){
-    const li = document.createElement('li');
-    li.textContent = `${amigo} tirou ${sorteado}`;
-    resultado.appendChild(li);
-}
-document.querySelector('.button-draw').addEventListener('click', sortearAmigo);
+// Para sortear um amigo
+    const indiceSorteado = Math.floor(Math.random()* amigos.length);
+    const amigoSorteado = amigos (indiceSorteado);
 
-
+// Para exibir o resultado
+    resultado.textContent = `O amigo secreto sorteado é: ${amigoSorteado} `;
+}
